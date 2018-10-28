599f918d63132bb1998aaa7cc894237366eedf18
cao <925454721@qq.com>
2018-10-28 20:07:32 +0800

    第二次修改

--- src/main/java/cn/itCao/service/HelloService.java
+++ src/main/java/cn/itCao/service/HelloService.java
@@ -1,5 +1,14 @@
 package cn.itCao.service;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class HelloService {
+	
+	@Test
+	public void testName() throws Exception {
+		
+	}
 
 }
